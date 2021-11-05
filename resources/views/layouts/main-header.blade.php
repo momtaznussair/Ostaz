c236cc74325b7537de0760412fad530ca0ee0cc1
momtaz <momtaz_nussair@yahoo.com>
2021-11-06 00:31:17 +0200

    installed mcamara

--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -11,18 +11,10 @@
 			</div>
 		</div>
 		<div class="main-header-right">
-			{{-- switch language
-			<ul class="nav">
-				@if (App::isLocale('en'))
-				<a class="dropdown-item" rel="alternate" hreflang="ar" href="{{ LaravelLocalization::getLocalizedURL('ar', null, [], true) }}">
-					<img src="{{ URL::asset('assets/img/flags/egypt-flag.png') }}" alt="العربية" title="العربية">
-				</a>
-				@else
-				<a class="dropdown-item" rel="alternate" hreflang="en" href="{{ LaravelLocalization::getLocalizedURL('en', null, [], true) }}">
-					<img  src="{{ URL::asset('assets/img/flags/us_flag.jpg') }}" style="width: 32px;" alt="English" title="English">
-				</a>
-				@endif
-			</ul> --}}
+			
+			{{-- switch language --}}
+			@include('partials.change-locale')
+
 			<div class="nav nav-item  navbar-nav-right ml-auto">
 				<div class="nav-item full-screen fullscreen-button">
 					<a class="new nav-link full-screen-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" class="header-icon-svgs" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-maximize"><path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"></path></svg></a>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group([
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+],
+function()
+{
+	/** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
+
+    Route::group([
+        'prefix' => 'admin',
+        'namespace' => 'Admin',
+
+    ],
+    function ()
+    {
+	/** ADD ALL Admin ROUTES INSIDE THIS GROUP **/
+
+    Route::view('/', 'admin.home');
+        
+    });
+	
 });
