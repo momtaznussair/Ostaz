b62182f3f0d36b92a3d9fd7166354111afc51342
momtaz <momtaz_nussair@yahoo.com>
2021-11-05 23:55:42 +0200

    setup and dashboard

---
+++ resources/views/layouts/main-header.blade.php
@@ -0,0 +1,61 @@
+<!-- main-header opened -->
+<div class="main-header sticky side-header nav nav-item">
+	<div class="container-fluid">
+		<div class="main-header-left ">
+			<div class="responsive-logo">
+				{{-- logo --}}
+			</div>
+			<div class="app-sidebar__toggle" data-toggle="sidebar">
+				<a class="open-toggle" href="#"><i class="header-icon fe fe-align-left" ></i></a>
+				<a class="close-toggle" href="#"><i class="header-icons fe fe-x"></i></a>
+			</div>
+		</div>
+		<div class="main-header-right">
+			{{-- switch language
+			<ul class="nav">
+				@if (App::isLocale('en'))
+				<a class="dropdown-item" rel="alternate" hreflang="ar" href="{{ LaravelLocalization::getLocalizedURL('ar', null, [], true) }}">
+					<img src="{{ URL::asset('assets/img/flags/egypt-flag.png') }}" alt="العربية" title="العربية">
+				</a>
+				@else
+				<a class="dropdown-item" rel="alternate" hreflang="en" href="{{ LaravelLocalization::getLocalizedURL('en', null, [], true) }}">
+					<img  src="{{ URL::asset('assets/img/flags/us_flag.jpg') }}" style="width: 32px;" alt="English" title="English">
+				</a>
+				@endif
+			</ul> --}}
+			<div class="nav nav-item  navbar-nav-right ml-auto">
+				<div class="nav-item full-screen fullscreen-button">
+					<a class="new nav-link full-screen-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" class="header-icon-svgs" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-maximize"><path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"></path></svg></a>
+				</div>
+				<div class="dropdown main-profile-menu nav nav-item nav-link">
+					@if (false) {{--Auth('admin')->user()->avatar--}}
+						<a class="profile-user d-flex" href=""><img alt="" src="{{asset('storage/' . Auth('admin')->user()->avatar)}}""></a>
+					@else
+						<a class="profile-user d-flex" href=""><img alt="" src="{{URL::asset('assets/img/faces/6.jpg')}}"></a>
+					@endif
+					<div class="dropdown-menu">
+						<div class="main-header-profile bg-primary p-3">
+							<div class="d-flex wd-100p">
+								<div class="main-img-user">
+								@if (false) {{-- Auth('admin')->user()->avatar --}}
+									<a class="profile-user d-flex" href=""><img alt="" src="{{asset('storage/' . Auth('admin')->user()->avatar)}}""></a>
+								@else
+									<a class="profile-user d-flex" href=""><img alt="" src="{{URL::asset('assets/img/faces/6.jpg')}}"></a>
+								@endif
+								</div>
+								<div class="mr-3 my-auto">
+									{{-- <h6 class="ml-2">{{Auth('admin')->user()->name}}</h6> --}}
+								</div>
+							</div>
+						</div>
+						<a class="dropdown-item" href="#" onclick="event.preventDefault();document.getElementById('logout-form').submit();"><i class="bx bx-log-out"></i>{{__('login.Sign Out')}}</a>
+						<form action="#" method="POST" id="logout-form">
+							@csrf
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- /main-header -->
---
+++ resources/views/layouts/main-sidebar.blade.php
@@ -0,0 +1,26 @@
+<!-- main-sidebar -->
+		<div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+		<aside class="app-sidebar sidebar-scroll">
+			<div class="main-sidebar-header active">
+				{{-- logo --}}
+			</div>
+			<div class="main-sidemenu">
+				<div class="app-sidebar__user clearfix">
+					<div class="dropdown user-pro-body">
+						<div class="">
+							<img alt="user-img" class="avatar avatar-xl brround" src="{{URL::asset('assets/img/faces/6.jpg')}}"><span class="avatar-status profile-status bg-green"></span>
+						</div>
+						<div class="user-info">
+							<h4 class="font-weight-semibold mt-3 mb-0">Petey Cruiser</h4>
+							<span class="mb-0 text-muted">Momtaz Nussair</span>
+						</div>
+					</div>
+				</div>
+				<ul class="side-menu">
+					<li class="slide mt-3">
+						<a class="side-menu__item" href="/"><svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24" ><path d="M0 0h24v24H0V0z" fill="none"/><path d="M5 5h4v6H5zm10 8h4v6h-4zM5 17h4v2H5zM15 5h4v2h-4z" opacity=".3"/><path d="M3 13h8V3H3v10zm2-8h4v6H5V5zm8 16h8V11h-8v10zm2-8h4v6h-4v-6zM13 3v6h8V3h-8zm6 4h-4V5h4v2zM3 21h8v-6H3v6zm2-4h4v2H5v-2z"/></svg><span class="side-menu__label">Main</span></a>
+					</li>
+				</ul>
+			</div>
+		</aside>
+<!-- main-sidebar -->
